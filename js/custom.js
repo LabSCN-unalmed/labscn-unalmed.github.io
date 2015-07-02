0ebf74f8319f8ab1f25a6ed41685bb5cca0a8608
caesarivs <caesarivs@gmail.com>
2015-07-02 15:59:35 -0500

    Mejoras en el TOC para las clases

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,21 @@
-toc = $("ul#toc");
-$("#content").find("h2").each(function() {
-  h = $(this);
-  toc.append("<li><a href='#" + h.attr("id") + "'>" + h.text() + "</a></li>");
+// toc = $("ul#toc");
+// $("#content").find("h2").each(function() {
+//   h = $(this);
+//   toc.append("<li><a href='#" + h.attr("id") + "'>" + h.text() + "</a></li>");
+// });
+
+$('#toc').toc({
+    listType: 'ul',
+    noBackToTopLinks: true,
+    headers: 'h2, h3',
+    title: ''
 });
 
 $('#sidebar').affix({
   offset: {
     top: 200
   }
-})
+});
 
 $(document.body).scrollspy({
   target: '#rightCol'
