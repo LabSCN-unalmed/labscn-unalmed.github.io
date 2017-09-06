acdee720bf894d73b27713eff764318bcf4fdfe3
Kenneth Roy Cabrera Torres <krcabrer@gmail.com>
2017-09-06 03:41:24 -0500

    Actualiza modulo R (1)

---
+++ _modulo-R/slides/datos.R
@@ -0,0 +1,6 @@
+x <-
+"un objeto"
+y <-
+structure(list(a = c(6L, 5L, 4L, 3L, 2L, 1L), b = structure(c(6L, 
+5L, 4L, 3L, 2L, 1L), .Label = c("a", "b", "c", "d", "e", "f"), class = "factor")), .Names = c("a", 
+"b"), row.names = c(NA, -6L), class = "data.frame")
