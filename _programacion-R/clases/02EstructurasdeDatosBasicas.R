a96b6b91bc6e55b159d46ccd68811205136f73e3
krcabrer <krcabrer@unal.edu.co>
2021-11-22 10:14:49 -0500

    Actualiza código de estructuras

--- _programacion-R/clases/02EstructurasdeDatosBasicas.R
+++ _programacion-R/clases/02EstructurasdeDatosBasicas.R
@@ -9,39 +9,39 @@
 
 # Vectores
 
-v1 <- c(2, 3, 5) 
+v1 <- c(2, 3, 5)
 class(v1)
 typeof(v1)
 
-v2 <- c(TRUE, FALSE, TRUE, FALSE, FALSE) 
+v2 <- c(TRUE, FALSE, TRUE, FALSE, FALSE)
 class(v2)
 typeof(v2)
 
-v3 <-  c("aa", "bb", "cc", "dd", "ee") 
+v3 <-  c("aa", "bb", "cc", "dd", "ee")
 class(v3)
 typeof(v3)
 
-n <- c(2, 3, 5) 
-s <- c("aa", "bb", "cc", "dd", "ee") 
-c(n, s) 
+n <- c(2, 3, 5)
+s <- c("aa", "bb", "cc", "dd", "ee")
+c(n, s)
 
-a <- c(1, 3, 5, 7) 
+a <- c(1, 3, 5, 7)
 b <- c(1, 2, 4, 8)
 
 5 * a
-a + b 
-a - b 
-a * b 
+a + b
+a - b
+a * b
 a / b
 
-u <- c(10, 20, 30) 
-v <- c(1, 2, 3, 4, 5, 6, 7, 8, 9) 
+u <- c(10, 20, 30)
+v <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
 u + v
 
-s <- c("aa", "bb", "cc", "dd", "ee") 
+s <- c("aa", "bb", "cc", "dd", "ee")
 s[3]
 
-s[-3] 
+s[-3]
 
 s[10]
 
@@ -49,21 +49,21 @@ s[c(2, 3)]
 
 s[c(2, 3, 3)]
 
-s[c(2, 1, 3)] 
+s[c(2, 1, 3)]
 
 s[2:4]
 
 help(":")
 
-L <- c(FALSE, TRUE, FALSE, TRUE, FALSE) 
-s[L] 
+L <- c(FALSE, TRUE, FALSE, TRUE, FALSE)
+s[L]
 
-s[c(FALSE, TRUE, FALSE, TRUE, FALSE)] 
+s[c(FALSE, TRUE, FALSE, TRUE, FALSE)]
 
-v <- c("Maria", "Susana") 
-v 
+v <- c("Maria", "Susana")
+v
 
-names(v) <- c("Primera", "Última") 
+names(v) <- c("Primera", "Última")
 v
 
 v["Primera"]
@@ -114,50 +114,50 @@ meses <- factor(meses, levels = c("enero", "febrero", "abril"),
 meses
 as.integer(meses)
 
-                
+
 # Matrices
 
-A <- matrix(c(2, 4, 3, 1, 5, 7), nrow=2, ncol=3, byrow = TRUE)        
+A <- matrix(c(2, 4, 3, 1, 5, 7), nrow=2, ncol=3, byrow = TRUE)
 
 A
 
 A[2, 3]
 
-A[2, ] 
+A[2, ]
 
 A[, 3]
 
-dimnames(A) = list( 
-     c("fila1", "fila2"),        
+dimnames(A) <- list(
+     c("fila1", "fila2"),
      c("col1", "col2", "col3"))
 A
 
 A["fila2","col3"]
 
-B <- matrix(c(2, 4, 3, 1, 5, 7), nrow=3, ncol=2) 
+B <- matrix(c(2, 4, 3, 1, 5, 7), nrow=3, ncol=2)
 B
 
 t(B)
 
 
-C <- matrix(c(7, 4, 2), nrow=3, ncol=1) 
-C 
+C <- matrix(c(7, 4, 2), nrow=3, ncol=1)
+C
 
 cbind(B, C)
 
-D <- matrix(c(6, 2), nrow=1,  ncol=2) 
-D 
+D <- matrix(c(6, 2), nrow=1,  ncol=2)
+D
 
-rbind(B, D) 
+rbind(B, D)
 
-c(B) 
+c(B)
 # Listas
 
-n <- c(2, 3, 5) 
-s <- c("aa", "bb", "cc", "dd", "ee") 
-b <- c(TRUE, FALSE, TRUE, FALSE, FALSE) 
+n <- c(2, 3, 5)
+s <- c("aa", "bb", "cc", "dd", "ee")
+b <- c(TRUE, FALSE, TRUE, FALSE, FALSE)
 x <- list(n, s, b, 3)
- 
+
 x[2]
 
 x[c(2, 4)]
@@ -170,8 +170,8 @@ x[[2]] <- "ta"
 s
 
 
-v <- list(numeros=c(2, 3, 5), nombres=c("aa", "bb")) 
-v 
+v <- list(numeros=c(2, 3, 5), nombres=c("aa", "bb"))
+v
 
 v["numeros"]
 
@@ -188,9 +188,9 @@ detach(v)
 
 # Bases de datos (data.frames)
 
-n <- c(2, 3, 5) 
-s <- c("aa", "bb", "cc") 
-b <- c(TRUE, FALSE, TRUE) 
+n <- c(2, 3, 5)
+s <- c("aa", "bb", "cc")
+b <- c(TRUE, FALSE, TRUE)
 df <- data.frame(n, s, b)
 
 
@@ -224,7 +224,7 @@ mtcars["camaro Z28", ]
 
 mtcars[c("Datsun 710", "Camaro Z28"), ]
 
-L = mtcars$am == 0
+L <- mtcars$am == 0
 L
 
 mtcars[L, ]
@@ -264,7 +264,7 @@ str(dfm)
 dfm[2, "y"]
 
 # Se debe tener mucho cuidado al utilizar estas opciones en las
-# bases de datos porque muchas funciones asumen que cada columa de 
+# bases de datos porque muchas funciones asumen que cada columa de
 # una base de datos es de tipo atómico.
 
 # Atributos de objetos (estructura de datos) en R.
@@ -299,5 +299,3 @@ class(modeloLineal1)
 # 3. class(): Aplica a cualquier objeto.
 # Se llaman directamente con su función respectiva,
 # los demás atributos se utiiza la función attributes() y attr().
-
-
